09c3bc8d1e2b5a9db549ede43ac40689279e0f3d
rrenusree <renusree@uw.edu>
2024-03-03 17:08:53 -0800

    my changes

--- huskysync/src/App.js
+++ huskysync/src/App.js
@@ -4,26 +4,39 @@ import React from 'react';
 import { useState } from 'react';
 import NavBar from './components/Navbar';
 import Team from './components/Team';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import Home from './Home.js';
 import GroupMain from "./GroupMain.js";
 import Settings from './Settings';
+import Login from './Login';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <BrowserRouter>
       <div className="App">
-        <NavBar />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/GroupMain" element={<GroupMain/>}/>
-          <Route path="/Settings" element={<Settings/>}/>
-        </Routes>
+        <Route path="/login" element={<Login/>}/>
+        {isLoggedIn ? (
+          <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/Home" element={<Home/>} />
+            <Route path="/GroupMain" element={<GroupMain/>}/>
+            <Route path="/Settings" element={<Settings/>}/>
+          </Routes>
+          </div>
+        ): (
+          <Navigate to="/login"/>
+        )
+      
+      }
       </div>
+      </Routes>
     </BrowserRouter>
   );
 }
