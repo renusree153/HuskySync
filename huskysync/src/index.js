0feee13c42c4433aa51dac1714e4f9e95cd4cd6c
thomas-kanenenaga <thomas.kanenaga@gmail.com>
2024-04-18 20:42:25 -0700

    unstyled new login page

--- huskysync/src/index.js
+++ huskysync/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import '@aws-amplify/ui-react/styles.css'
+//import '@aws-amplify/ui-react/styles.css'
 import { Amplify } from 'aws-amplify'; // Updated import
 import config from "./aws-exports"
 import { ThemeProvider } from '@aws-amplify/ui-react';
