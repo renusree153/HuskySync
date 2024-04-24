b97e6243064018814cae44c83731f0a22527e45b
varshapalepu <palepvar@uw.edu>
2024-04-24 12:45:39 -0700

    faq qs

--- huskysync/src/FAQ.js
+++ huskysync/src/FAQ.js
@@ -9,22 +9,34 @@ import { BrowserRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function FAQ() {
-    const [username, setUsername] = useState('');
-    return (
-        <div className="FAQ">
-          <NavBar/>
-          <header className="FAQ-header">
-            <h1>
-                FAQ
-            </h1>
-            <h2>
-                Coming Soon.
-            </h2>
-  
-          </header>
+  const [username, setUsername] = useState('');
+  return (
+    <div>
+      <NavBar />
+      <header className="FAQ-header">
+          <h1>
+            We know studying can be hard, and we want to help!
+          </h1>
+      </header>
+      <div className="demo-title">
+        <h2>
+          Quizzes
+        </h2>
+          <ul> How do I create one? </ul>
+            <p> On the left hand of the quiz page, enter the details of the quiz you would like to create. Click Create and promptly upload your study notes.</p>
+          <ul> If I create a quiz, am I responsible for anything else? </ul>
+            <p> No! After you create a quiz, you are free to take it as if someone else created it. This means it’s totally okay if you are not able to make it. At the end of the day, your quiz will help other classmates study as well!</p>
+          <ul> Are my quizzes public? </ul>
+            <p> Yes, all quizzes are inherently public to allow other users access to study material.</p>
+        <h2>
+          Groups
+        </h2>
+          <ul> Is there a minimum or maximum for groups? </ul>
+            <p> 1-5 students per quiz is the current min & max. As popularity increases we plan to increase the maximum number per quiz!</p> 
         </div>
-    );
-  }
-  
-  export default FAQ;
+    </div>
+  );
+ } 
+
+export default FAQ; 
 
