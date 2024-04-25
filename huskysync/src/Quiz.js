be3dba95efe75116f4c643c986cd70c91c817a92
Anumita Ghosh <aghosh7@uw.edu>
2024-04-25 13:58:32 -0700

    updated styling

--- huskysync/src/Quiz.js
+++ huskysync/src/Quiz.js
@@ -18,13 +18,16 @@ function Quiz() {
             </div>
             <div class="quiz-progress">
                 <ul>
-                    <li>1</li>
+                    <li id="q1">1</li>
                     <li>2</li>
                     <li>3</li>
                     <li>4</li>
                     <li>5</li>
                     <li>6</li>
                     <li>7</li>
+                    <li>8</li>
+                    <li>9</li>
+                    <li>10</li>
                 </ul>
             </div>
             <div className="quiz-content">
