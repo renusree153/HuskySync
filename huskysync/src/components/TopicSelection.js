874760922b0d85624f4c3244c899f6a4df8f26e7
varshapalepu <palepvar@uw.edu>
2024-04-29 10:56:08 -0700

    topicselection

---
+++ huskysync/src/components/TopicSelection.js
@@ -0,0 +1,8 @@
+import './TopicSelection.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import awsconfig from '../aws-exports';
+import {Route, Routes} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+
