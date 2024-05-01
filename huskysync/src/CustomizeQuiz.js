0d1c99e5dde59560391bacac8c9c9ae3b986259a
varshapalepu <palepvar@uw.edu>
2024-05-01 14:06:36 -0700

    styling

--- huskysync/src/CustomizeQuiz.js
+++ huskysync/src/CustomizeQuiz.js
@@ -3,12 +3,17 @@ import React from 'react';
 import { useState } from 'react';
 import NavBar from './components/Navbar';
 import { Link } from 'react-router-dom';
-
 function CustomizeQuiz() {
     const [numQuestions, setNumQuestions] = useState('5'); // Default to 5 questions
 
     const handleNumQuestionsChange = (event) => {
         setNumQuestions(event.target.value);
+    };
+
+    const [quizLength, setQuizLength] = useState('10'); // Default to 10 minutes
+
+    const handleQuizLengthChange = (event) => {
+        setQuizLength(event.target.value);
     };
 
     return (
@@ -17,12 +22,12 @@ function CustomizeQuiz() {
                 <h2 id="custom-quiz-title">PHYS 121: Electromagnetism</h2>
             </div>
             <div className='header'>
-                <h2 id="titlet"> Questions</h2>
+                <h2 id="titlet"> Customization:</h2>
             </div>
-            <div className="settings-container">
+            <div className="settings-container create-quiz-container">
                 <div className="settings">
                     <div className="setting-item">
-                        <label>How Many?</label>
+                        <label>How Many Questions?</label>
                         <select value={numQuestions} onChange={handleNumQuestionsChange}>
                             <option value="5">5</option>
                             <option value="10">10</option>
@@ -32,7 +37,18 @@ function CustomizeQuiz() {
                     </div>
 
                     <div className="setting-item">
-                        <label>What Type?</label>
+                        <label>How Many Minutes?</label>
+                        <select value={quizLength} onChange={handleQuizLengthChange}>
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                            <option value="40">40</option>
+                        </select>
+                    </div>
+
+                    <div className="setting-item">
+                        <label>What Types of Questions?</label>
+                        <div > 
                         <div>
                             <input
                                 type="checkbox"
@@ -42,7 +58,7 @@ function CustomizeQuiz() {
                             />
                             <label htmlFor="trueOrFalse">True or False</label>
                         </div>
-                        <div>
+                        <div className="align">
                             <input
                                 type="checkbox"
                                 id="multipleChoice"
@@ -50,6 +66,7 @@ function CustomizeQuiz() {
                                 // Add checked logic here if needed
                             />
                             <label htmlFor="multipleChoice">Multiple Choice</label>
+                        </div>
                         </div>
                     </div>
                 </div>
